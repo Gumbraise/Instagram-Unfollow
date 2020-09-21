5fa13db35ac2b9c50ca139721fa732bd1a10106b
Kellis <vikiclashonyt@gmail.com>
2020-09-21 21:50:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ password = input("Put your IG Password then press ENTER: ")
 api = InstagramAPI(username, password)
 api.login()
 
-following = api.getSelfUsersFollowing()
-
-for item in following["users"]:
-    user = item["pk"]
-    username = item["username"]
+following = api.getTotalSelfFollowings()
+for k, v in enumerate(following):
+    user = v['pk']
+    username = v["username"]
 
     api.unfollow(user)
     print(username + " is now unfollowed")
